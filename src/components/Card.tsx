3dbc670b9f9782a615290b73f2665971968517b4
Mark Shenouda <mark@thisdot.co>
2022-07-14 14:15:27 +0200

    Added card

---
+++ src/components/Card.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+interface Props {
+  title: string;
+  description?: string;
+  image: string;
+  slug: string;
+  readTime: string;
+}
+
+function Card({ title, description, image, slug, readTime }: Props) {
+  return (
+    <Link href={slug}>
+      <a>
+        <div className="m-2">
+          <img
+            src={image}
+            alt=""
+            className={` w-full rounded-xl object-cover ${
+              description ? "h-[500px]" : "h-56"
+            }`}
+          />
+          <h2 className="mt-2 text-2xl font-bold">{title}</h2>
+          <p className="text-2xl text-gray-600">{description}</p>
+        </div>
+      </a>
+    </Link>
+  );
+}
+
+export default Card;
