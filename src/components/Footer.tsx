8f84999c2716be322454b2dee106849a486f08e7
Mark Shenouda <mark@thisdot.co>
2022-07-14 14:13:00 +0200

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="border-t bg-slate-100 p-2 py-16 text-center text-slate-600">
+      &copy; {new Date().getFullYear()} My Blog
+    </footer>
+  );
+}
+
+export default Footer;
