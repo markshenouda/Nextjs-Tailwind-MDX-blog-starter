868c727ba02203d9f6c045a4da03b37ae27a7807
Mark Shenouda <mark@thisdot.co>
2022-07-14 14:15:15 +0200

    added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,24 +5,24 @@ import { FiTwitter } from "react-icons/fi";
 function Header() {
   return (
     <div className="border-b">
-      <header className="flex items-center justify-between py-8 text-2xl container mx-auto">
+      <header className="container mx-auto flex items-center justify-between py-8 text-2xl">
         <div className="flex items-center gap-3">
-          <img src="/logo.jpeg" alt="My Blog" className="w-12 h-12 rounded-full" />
+          <img
+            src="/logo.jpeg"
+            alt="My Blog"
+            className="h-12 w-12 rounded-full"
+          />
           <Link href="/">
             <a className="font-medium">My Blog</a>
           </Link>
         </div>
         <div className="flex gap-4 text-gray-600">
-          <nav className="text-lg flex gap-4 mr-8">
+          <nav className="mr-8 flex gap-4 text-lg">
             <Link href="/">
-              <a className="text-gray-600 hover:text-gray-900">
-                Blog
-              </a>
+              <a className="text-gray-600 hover:text-gray-900">Blog</a>
             </Link>
             <Link href="/">
-              <a className="text-gray-600 hover:text-gray-900">
-                About
-              </a>
+              <a className="text-gray-600 hover:text-gray-900">About</a>
             </Link>
           </nav>
           <button className="text-gray-600 hover:text-gray-700">
