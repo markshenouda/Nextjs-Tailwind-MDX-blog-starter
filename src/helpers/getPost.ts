1e3f4c589a3db82dac07fcb84b92d01c4f4b4c69
Mark Shenouda <mark@thisdot.co>
2022-07-23 01:36:42 +0200

    added getPost helper

--- src/helpers/getPost.ts
+++ src/helpers/getPost.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 import matter from "gray-matter";
 
-const getPost = (slug: String) => {
+const getPost = async (slug: string) => {
   const fullPath = path.join(process.cwd(), "data/posts", `${slug}.mdx`);
   const fileContents = readFileSync(fullPath, "utf8");
 
