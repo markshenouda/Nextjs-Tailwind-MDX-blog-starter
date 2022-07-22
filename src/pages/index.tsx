54fc30b5e741b199d4606095e5dddd605d16758e
Mark Shenouda <mark@thisdot.co>
2022-07-23 01:41:03 +0200

    hook index page to real data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,36 @@
 import type { NextPage } from "next";
 import Card from "@/components/Card";
+import getAllPostsData from "@/helpers/getAllPostsData";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: {
+    slug: string;
+    data: {
+      [key: string]: any;
+    };
+    readTime: number;
+  }[];
+}
+
+const Home: NextPage<Props> = ({ posts }) => {
+  console.log(posts);
   return (
     <div className="p-16 ">
+      <noscript>Hello you should turn on JavaScript</noscript>
       <div className="mb-16 grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <Card
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
-            image="/demo.jpeg"
-            slug="/posts/test"
-            readTime="6"
-            description="Repudiandae illum aperiam debitis aut rem labore laboriosam praesentium sequi recusandae, hic error consequuntur nisi. Repellat totam itaque quis sit accusamus possimus."
-          />
+          {posts[0] && (
+            <Card
+              title={posts[0].data.title}
+              image="/demo.jpeg"
+              slug="/posts/markdown-guide"
+              readTime="6"
+              description="Repudiandae illum aperiam debitis aut rem labore laboriosam praesentium sequi recusandae, hic error consequuntur nisi. Repellat totam itaque quis sit accusamus possimus."
+            />
+          )}
         </div>
         <div className="col-span-1 flex flex-col gap-4">
-          <Card
+          {/* <Card
             title="Lorem ipsum dolor sit, amet consectetur."
             image="/demo.jpeg"
             slug="/test"
@@ -26,15 +41,25 @@ const Home: NextPage = () => {
             image="/demo.jpeg"
             slug="/test"
             readTime="6"
-          />
+          /> */}
+          {posts.slice(1, 3).map((post) => (
+            <Card
+            key={post.slug}
+              title={post.data.title}
+              image="/demo.jpeg"
+              slug={`/posts/${post.slug}`}
+              readTime="6"
+            />
+          ))}
         </div>
       </div>
       <div className="grid grid-cols-3 gap-8">
-        {Array.from({ length: 10 }).map((_, i) => (
+        {posts.slice(3).map((post) => (
           <Card
-            title="Lorem ipsum dolor sit, amet consectetur elit."
+            key={post.slug}
+            title={post.data.title}
             image="/demo.jpeg"
-            slug="/test"
+            slug={`/posts/${post.slug}`}
             readTime="6"
           />
         ))}
@@ -44,3 +69,12 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const posts = getAllPostsData();
+  return {
+    props: {
+      posts,
+    },
+  };
+};
