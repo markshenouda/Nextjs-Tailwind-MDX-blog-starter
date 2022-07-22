55c76073966088a03eb59100035e1ad2741111af
Mark Shenouda <mark@thisdot.co>
2022-07-23 01:56:18 +0200

    return read time for each post

--- src/helpers/getPost.ts
+++ src/helpers/getPost.ts
@@ -1,15 +1,18 @@
 import { readFileSync } from "fs";
 import path from "path";
 import matter from "gray-matter";
+import getReadTime from "./getReadTime";
 
 const getPost = async (slug: string) => {
   const fullPath = path.join(process.cwd(), "data/posts", `${slug}.mdx`);
   const fileContents = readFileSync(fullPath, "utf8");
 
   const { data, content } = matter(fileContents);
+  const readTime = getReadTime(content);
   return {
     data,
     content,
+    readTime,
   };
 };
 
