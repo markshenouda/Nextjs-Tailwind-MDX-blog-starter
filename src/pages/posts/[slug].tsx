6bb60d58ac882f8e531a46511963cd0eacd118e5
Mark Shenouda <mark@thisdot.co>
2022-07-23 01:40:38 +0200

    Added to post page - getStaticProps - getStaticPaths - remarkGfm

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,53 +1,48 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps, GetStaticPaths } from "next";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import getPost from "@/helpers/getPost";
+import getAllPostsData from "@/helpers/getAllPostsData";
+import remarkGfm from 'remark-gfm';
+import { serialize } from "next-mdx-remote/serialize";
 
-const PostPage: NextPage = () => {
+interface Props {
+  data: [key: string, value: any];
+  content: MDXRemoteSerializeResult;
+}
+
+const PostPage: NextPage<Props> = ({ data, content }) => {
   return (
     <div>
       <article className="prose mx-auto my-16 dark:prose-invert lg:prose-xl">
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel dolore
-          deleniti perferendis, odit voluptatum asperiores vitae est saepe
-          ullam. Magnam sit alias excepturi doloribus ducimus tempore porro, non
-          dolores possimus?
-        </p>
-
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel dolore
-          deleniti perferendis, odit voluptatum asperiores vitae est saepe
-          ullam. Magnam sit alias excepturi doloribus ducimus tempore porro, non
-          dolores possimus?
-        </p>
-        <pre>
-          <code className="language-jsx">
-            {`import React from "react";
-import { useRouter } from "next/router";
-import { useQuery } from "react-query";
-import { usePosts } from "../../hooks/usePosts";
-import { Post } from "../../components/Post";
-import { PostPageQuery } from "../../types/PostPageQuery";
-import { PostPage } from "../../types/PostPage";
-import { usePostsQuery } from "../../hooks/usePostsQuery";`}
-          </code>
-        </pre>
-
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel dolore
-          deleniti perferendis, odit voluptatum asperiores vitae est saepe
-          ullam. Magnam sit alias excepturi doloribus ducimus tempore porro, non
-          dolores possimus? Lorem ipsum dolor, sit amet consectetur adipisicing
-          elit. Vel dolore deleniti perferendis, odit voluptatum asperiores
-          vitae est saepe ullam. Magnam sit alias excepturi doloribus ducimus
-          tempore porro, non dolores possimus? Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Vel dolore deleniti perferendis, odit
-          voluptatum asperiores vitae est saepe ullam. Magnam sit alias
-          excepturi doloribus ducimus tempore porro, non dolores possimus?
-        </p>
+        <MDXRemote {...content} />
       </article>
     </div>
   );
 };
 
 export default PostPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = getAllPostsData();
+  return {
+    paths: posts.map(({ slug }) => ({ params: { slug } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if(!params) {
+    return { props: { data: [], content: {} } };
+  }
+  const { content, data } = await getPost(params.slug as string);
+  return {
+    props: {
+      content: await serialize(content, {
+        mdxOptions: {
+          remarkPlugins: [remarkGfm]
+        },
+      }),
+      data,
+    },
+  };
+};
